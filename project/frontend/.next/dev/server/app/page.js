6427f564c288065a6932ec95171e0b477a6d896a
“Tonmaira” <mai.tan.2888@gmail.com>
2026-08-22 19:36:43 +0700

    update:services box eliminate

--- project/frontend/.next/dev/server/app/page.js
+++ project/frontend/.next/dev/server/app/page.js
@@ -1,6 +1,6 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
-R.c("server/chunks/ssr/node_modules_d91377ac._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__2dc0783f._.js")
+R.c("server/chunks/ssr/node_modules_1adb9fd0._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__154c98fa._.js")
 R.c("server/chunks/ssr/node_modules_e2d1c5df._.js")
 R.c("server/chunks/ssr/[externals]__e8a2741f._.js")
 R.c("server/chunks/ssr/app_b9b1292a._.js")
